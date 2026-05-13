e4e023bf5947df83b4a29051a1db33d4fe6ab16d
amanzeao7 <amanzeao7@gmail.com>
2026-05-13 12:46:08 +0100

    Update page.tsx

--- app/treatments/page.tsx
+++ app/treatments/page.tsx
@@ -1,7 +1,9 @@
+
+
 import type { Metadata } from 'next'
 import TreatmentsClient from './TreatmentsClient'
-import Nav from '../components/Nav'
-import Footer from '../components/Footer'
+import Nav from '@/app/components/Nav'
+import Footer from '@/app/components/Footer'
 import { getSiteSettings } from '@/lib/sanity'
 import { PLACEHOLDER_SETTINGS } from '@/lib/placeholder-data'
 
